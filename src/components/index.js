4bbf7c571be44cfb2decc0904dc5103a36dc298e
Joy Ahmed <esthersboydd7806@gmail.com>
2022-09-18 00:10:48 +0600

    intial files with data's

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import NavBar from "./NavBar";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Billing from "./Billing";
+import Footer from "./Footer";
+import CardDeal from "./CardDeal";
+import CTA from "./CTA";
+import Testimonials from "./Testimonials";
+import Clients from "./Clients";
+import Business from "./Business";
+
+export {
+  NavBar,
+  Hero,
+  Stats,
+  Billing,
+  Footer,
+  CardDeal,
+  CTA,
+  Testimonials,
+  Clients,
+  Business,
+}
